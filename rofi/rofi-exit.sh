75ceb617bd78c64bfa53593db47ff00f76346df2
Ethan Loi <z5479926@ad.unsw.edu.au>
2025-03-14 09:04:33 +1100

    Slight Fix to Exit Screen

--- rofi/rofi-exit.sh
+++ rofi/rofi-exit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-options="⏾  Suspend\n⏻  Power off\n🗘  Reboot"
-chosen_option=$(echo -e "$options" | rofi -dmenu -i -show-icons | tr "A-Z" "a-z" | tr -d "[:space:]")
+options="⏾ Suspend\n⏻ Power off\n🗘 Reboot"
+chosen_option=$(echo -e "$options" | rofi -dmenu -i -show-icons | tr "A-Z" "a-z" | tr -d "[:space:]" | grep -oE "[a-z]*")
 
 case "$chosen_option" in
 "poweroff" | "reboot") $("$chosen_option") ;;
